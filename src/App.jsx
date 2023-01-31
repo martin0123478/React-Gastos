25bae400849659b1c1c489a3dbd555eff440575c
martin0123478 <martin_agui97@outlook.es>
2023-01-30 21:31:38 -0600

    Creando componente de header y nuevo presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-
+import Header from './components/Header'
 
 function App() {
   
 
   return (
-    <h1>Control de gastos</h1>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
