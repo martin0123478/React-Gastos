d3ce1560ed26caf95095cad38c0ddefed02537e7
martin0123478 <martin_agui97@outlook.es>
2023-01-31 21:41:56 -0600

    Mostrando y ocultando el modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
 
 function App() {
   const [presupuesto,setPresupuesto] = useState(0)
   const [isValidPrespuesto, setIsvalidPresupuesto] = useState(false)
+  const [modal,setModal] = useState(false)
+
+  const handleNuevoGasto = () =>{
+    setModal(true)
+  }
 
   return (
     <div>
@@ -13,6 +20,17 @@ function App() {
         isValidPrespuesto = {isValidPrespuesto}
         setIsvalidPresupuesto = {setIsvalidPresupuesto}
       />
+
+
+    {isValidPrespuesto && (
+      <div className='nuevo-gasto'>
+      <img src={IconoNuevoGasto} alt="icono-NUEVO-GASTO" 
+      onClick={handleNuevoGasto}/>
+    </div>
+    )}
+
+    {modal && <Modal setModal = {setModal}/>}
+      
     </div>
   )
 }
