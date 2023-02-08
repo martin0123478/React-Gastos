5c7204e0b30040d075ae9b865c2c2dc60c0fbc10
martin0123478 <martin_agui97@outlook.es>
2023-02-07 21:02:21 -0600

    Creando el State para gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {useState} from 'react'
 import CerrarBtn from '../img/cerrar.svg'
 const Modal = ({setModal,animarModal,setAnimarModal}) => {
+
+    const [nombre,setNombre] = useState('')
+    const [cantidad,setCantidad] = useState(0)
+    const [categoria,setCategpria] = useState('')
+
 
     const ocultarModal = () =>{
         setModal(false)
@@ -24,6 +30,8 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
                 <label htmlFor="nombre">Nombre Gasto</label>
                 <input type="text"
                 id='nombre'
+                value={nombre}
+                onChange={e =>setNombre(e.target.value)}
                 placeholder='Añade el Nombre del Gasto' />
             </div>
 
@@ -31,12 +39,17 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
                 <label htmlFor="cantidad">Cantidad</label>
                 <input type="number"
                 id='cantidad'
+                value={cantidad}
+                onChange={e => setCantidad(Number(e.target.value))}
                 placeholder='Añade la cantidad del gasto: Ejemplo 300' />
             </div>
 
             <div className='campo'>
                 <label htmlFor="categoria">Categoria</label>
-                <select name="" id="categoria">
+                <select name="" id="categoria"
+                value={categoria}
+                onChange={e =>setCategpria(e.target.value)}
+                >
                 <option value="">--Seleccione--</option>
                 <option value="ahorro">Ahorro</option>
                 <option value="comida">Comida</option>
