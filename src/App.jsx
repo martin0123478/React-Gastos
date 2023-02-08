5d9e47490931b34f3a59375879158b9841b3eac5
martin0123478 <martin_agui97@outlook.es>
2023-02-07 21:30:59 -0600

    Validando gastos, añadiendo gastos al state, generando id

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import Header from './components/Header'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 import Modal from './components/Modal'
+import {generarId} from './helpers'
 
 function App() {
   const [presupuesto,setPresupuesto] = useState(0)
   const [isValidPrespuesto, setIsvalidPresupuesto] = useState(false)
   const [modal,setModal] = useState(false)
   const [animarModal,setAnimarModal] = useState(false)
+  const [gastos,setGastos] = useState([])
 
   const handleNuevoGasto = () =>{
     setModal(true)
@@ -15,6 +17,15 @@ function App() {
     setTimeout(() => {
       setAnimarModal(true)
     }, 500);
+  }
+  const guardarGasto = gasto =>{
+    gasto.id = generarId()
+    setGastos([...gastos,gasto])
+    setAnimarModal(false)
+
+        setTimeout(() => {
+            setModal(false)
+        }, 500);
   }
 
 
@@ -36,7 +47,8 @@ function App() {
     </div>
     )}
 
-    {modal && <Modal setModal = {setModal} animarModal = {animarModal} setAnimarModal={setAnimarModal}/>}
+    {modal && <Modal setModal = {setModal} animarModal = {animarModal} setAnimarModal={setAnimarModal}
+    guardarGasto = {guardarGasto}/>}
       
     </div>
   )
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {useState} from 'react'
+import Mensaje from './Mensaje'
 import CerrarBtn from '../img/cerrar.svg'
-const Modal = ({setModal,animarModal,setAnimarModal}) => {
+const Modal = ({setModal,animarModal,setAnimarModal,guardarGasto}) => {
+    const [mensaje,setMensaje] = useState('')
 
     const [nombre,setNombre] = useState('')
     const [cantidad,setCantidad] = useState(0)
@@ -16,6 +18,18 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
             setModal(false)
         }, 500);
     }
+
+    const handleSubmit = e =>{
+        e.preventDefault()
+        if([nombre,cantidad,categoria].includes('')){
+            setMensaje('Todos los Campos son obligatorios')
+            setTimeout(() => {
+                setMensaje('')
+            }, 3000);
+            return
+        }
+        guardarGasto({nombre,cantidad,categoria})
+    }
   return (
     <div className='modal'>
         <div className='cerrar-modal'>
@@ -24,8 +38,11 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
             />
         </div>
 
-        <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
+        <form 
+        onSubmit={handleSubmit}
+        className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
             <legend>Nuevo Gasto</legend>
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
             <div className='campo'>
                 <label htmlFor="nombre">Nombre Gasto</label>
                 <input type="text"
